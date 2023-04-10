92230c40aa8beeeed05595070ed765284470d617
Garv312 <garvy312@gmail.com>
2023-04-11 00:26:19 +0530

    correction

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Mannu Neko cute couple'
+    app.config['SECRET_KEY'] = 'weak'
+
+    
 
     return app
 
