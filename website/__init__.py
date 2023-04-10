00339b588c802e0b8990c82287a53fb4cf3a92bd
Garv312 <garvy312@gmail.com>
2023-04-10 17:41:09 +0530

    second commit

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Mannu Neko'
+    app.config['SECRET_KEY'] = 'Mannu Neko cute couple'
 
     return app
 
