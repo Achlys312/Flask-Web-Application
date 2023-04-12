7bc277b02971699496c5b5d6cdd66abdfc5789a8
Garv312 <garvy312@gmail.com>
2023-04-12 22:39:29 +0530

    commit7

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash 
+from . import db
 
 auth = Blueprint('auth', __name__)
  
@@ -27,6 +30,11 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account screated!', category='success')
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
